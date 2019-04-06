35a5c637b3abcb5466efeb50145d17ac14a8ba01
ipek <ipekcivicakan@gmail.com>
2019-04-05 23:59:05 -0700

    App Components Structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
+import styled from 'styled-components';
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr)
+`
+
 class App extends Component {
   render() {
     return (
-      <div>
-        Ipek
-      </div>
+      <Grid>
+       <ColorPanel/>
+       <SidePanel />
+       <Messages />
+       <MetaPanel />
+      </Grid>
     );
   }
 }
